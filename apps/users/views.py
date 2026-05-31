18d65fa3f4c463a134f0a58b7138e7a10caa9edb
Coolbit <neeoub@gmail.com>
2026-05-31 23:37:38 +0545

    Feat: Added daisyUI and tailwind for css part

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,24 +1,25 @@
 from django.db import IntegrityError
 from django.shortcuts import redirect, render
 from django.contrib.auth import get_user_model, authenticate, login, logout
+from django.contrib import messages
 
 User = get_user_model()
 
 
 def register_view(request):
-    if request.method == "POST":
+    
+    # TODO: NULL HANDLING / EMPTY VALUES HANDLING
+    
+    if request.method == "POST":        
         if User.objects.filter(username=request.POST.get("username")).exists():
-            return render(
-                request, "users/register.html", {"error": "Username already exists"}
-            )
+            messages.error(request, "Username already exists")
+            return redirect("register")
         if User.objects.filter(email=request.POST.get("email")).exists():
-            return render(
-                request, "users/register.html", {"error": "Email already exists"}
-            )
+            messages.error(request, "Email already exists")
+            return redirect("register")
         if request.POST.get("password") != request.POST.get("confirm_password"):
-            return render(
-                request, "users/register.html", {"error": "Passwords do not match"}
-            )
+            messages.error(request, "Passwords do not match")            
+            return redirect("register")
         try:
             user = User.objects.create_user(
                 username=request.POST.get("username"),
@@ -28,33 +29,33 @@ def register_view(request):
                 password=request.POST.get("password"),
             )
         except IntegrityError:
-            return render(
-                request,
-                "users/register.html",
-                {"error": "An error occurred while creating the user"},
-            )
+            messages.error(request, "An error occurred while creating the user")            
+            return redirect("register")
         login(request, user)
         return redirect("login")
     return render(request, "users/register.html")
 
 
 def login_view(request):
+    
+
     if request.method == "POST":
         username = request.POST.get("username")
         password = request.POST.get("password")
         email = request.POST.get("email")
+        if not password or (not username and not email):
+            messages.error(request, "Do not leave required fields empty!!")
+            return redirect("login")
         if email:
             try:
                 username = User.objects.get(email__exact=email).username
             except User.DoesNotExist:
-                return render(
-                    request, "users/login.html", {"error": "Account not found!!"}
-                )
+                messages.error(request, "Account not found!!")            
+                return redirect("login")
         user = authenticate(request, username=username, password=password)
         if not user:
-            return render(
-                request, "users/login.html", {"error": "Invalid Login Details!!"}
-            )
+            messages.error(request, "Invalid Login Details!!")            
+            return redirect("login")
         login(request, user)
         return redirect("register")
     return render(request, "users/login.html")
