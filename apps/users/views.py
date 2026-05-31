c536f0355efd25b331d322fd0ab6bde2badf0a16
Coolbit <neeoub@gmail.com>
2026-05-31 16:24:46 +0545

    Feat: Added login and logout views

--- apps/users/views.py
+++ apps/users/views.py
@@ -4,18 +4,62 @@ from django.contrib.auth import get_user_model, authenticate, login, logout
 
 User = get_user_model()
 
-def register(request):
-    if request.method == 'POST':
-        if User.objects.filter(username=request.POST['username']).exists():
-            return render(request, 'users/register.html', {'error': 'Username already exists'})
-        if User.objects.filter(email=request.POST['email']).exists():
-            return render(request, 'users/register.html', {'error': 'Email already exists'})
-        if request.POST['password'] != request.POST['confirm_password']:
-            return render(request, 'users/register.html', {'error': 'Passwords do not match'})
+
+def register_view(request):
+    if request.method == "POST":
+        if User.objects.filter(username=request.POST.get("username")).exists():
+            return render(
+                request, "users/register.html", {"error": "Username already exists"}
+            )
+        if User.objects.filter(email=request.POST.get("email")).exists():
+            return render(
+                request, "users/register.html", {"error": "Email already exists"}
+            )
+        if request.POST.get("password") != request.POST.get("confirm_password"):
+            return render(
+                request, "users/register.html", {"error": "Passwords do not match"}
+            )
         try:
-            user = User.objects.create_user(username=request.POST['username'], first_name=request.POST['first_name'], last_name=request.POST['last_name'], email=request.POST['email'], password=request.POST['password'])
+            user = User.objects.create_user(
+                username=request.POST.get("username"),
+                first_name=request.POST.get("first_name"),
+                last_name=request.POST.get("last_name"),
+                email=request.POST.get("email"),
+                password=request.POST.get("password"),
+            )
         except IntegrityError:
-            return render(request, 'users/register.html', {'error': 'An error occurred while creating the user'})
+            return render(
+                request,
+                "users/register.html",
+                {"error": "An error occurred while creating the user"},
+            )
         login(request, user)
-        return redirect('home')
-    return render(request, 'users/register.html')
+        return redirect("feed")
+    return render(request, "users/register.html")
+
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        email = request.POST.get("email")
+        if email:
+            try:
+                username = User.objects.get(email__exact=email).username
+            except User.DoesNotExist:
+                return render(
+                    request, "users/login.html", {"error": "Account not found!!"}
+                )
+        user = authenticate(request, username=username, password=password)
+        if not user:
+            return render(
+                request, "users/login.html", {"error": "Invalid Login Details!!"}
+            )
+        login(request, user)
+        return redirect("feed")
+    return render(request, "users/login.html")
+
+
+def logout_view(request):
+    logout(request)
+    return redirect("login")
