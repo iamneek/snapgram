c27f8488ddd9034ae70336d26fd9b8f7945065f4
Coolbit <neeoub@gmail.com>
2026-05-31 13:29:23 +0545

    Models seetup

--- apps/posts/models.py
+++ apps/posts/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.conf import settings
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+class Post(models.Model):
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="posts"
+    )
+    caption = models.CharField(max_length=200, blank=True)
+    image = CloudinaryField("image", blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.author.username}'s post: {self.caption}"
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="comments"
+    )
+    content = models.TextField(max_length=300)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.author.username} commented on {self.post.author.username}'s post: {self.content[:20]}..."
--- apps/users/models.py
+++ apps/users/models.py
@@ -1,5 +1,25 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from cloudinary.models import CloudinaryField
+
 
 class User(AbstractUser):
-    pass
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    USERNAME_FIELD = "username"
+    REQUIRED_FIELDS = ["email", "first_name", "last_name"]
+
+    def __str__(self):
+        return self.username
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
+    bio = models.TextField(blank=True)
+    avatar = CloudinaryField("avatar", blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.user.username}'s Profile"
