c77900d3eb3d892c088de2a63f377f2f07ec884e
Coolbit <neeoub@gmail.com>
2026-06-02 13:43:24 +0545

    Feat: Added Like Model

--- apps/posts/models.py
+++ apps/posts/models.py
@@ -27,3 +27,20 @@ class Comment(models.Model):
 
     def __str__(self):
         return f"{self.author.username} commented on {self.post.author.username}'s post: {self.content[:20]}..."
+
+
+class Like(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="likes")
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="likes"
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        # unique_together = ('post', 'user')
+        constraints = [
+            models.UniqueConstraint(fields=['post', 'user'], name='unq_post_like')
+        ]
+    
+    def __str__(self):
+        return f"{self.user.username} liked {self.post.author}'s post."
